3725f6fe7338dbaa20dfc9c8553d8b348c31b404
daivy <2259492701@qq.com>
2026-05-15 14:45:48 +0800

    repo(done for now)

--- rv32i-compiler/build/_preprocessed.c
+++ rv32i-compiler/build/_preprocessed.c
@@ -1,4 +1,30 @@
-int main() {
-    int x = 3 * 5;
-    return x;
+/* Phase 10 integration test: volatile + const + void + typedef + static */
+
+struct Point {
+    int x;
+    int y;
+};
+
+typedef struct Point Pt;
+
+static Pt global_pt;
+volatile int state = 0;
+const int VERSION = 10;
+
+void init_pt(Pt *p, int x, int y) {
+    p->x = x;
+    p->y = y;
+}
+
+int get_version(void) {
+    return VERSION;
+}
+
+int main(void) {
+    Pt *p = &global_pt;
+    init_pt(p, 5, 15);
+    state = 1;
+    int sum = p->x + p->y;
+    int ver = get_version();
+    return sum + ver + state;  /* 5+15+10+1 = 31 */
 }
